fd53f357858259ef9cd5372cee6f0e0e76f9f696
Tim Randall <trtimrandall@gmail.com>
2021-08-02 20:27:59 +1000

    Initial Commit

---
+++ SightReader/SightReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SightReaderApp.swift
+//  SightReader
+//
+//  Created by Tim Randall on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SightReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
